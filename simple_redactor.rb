4859b25da3474ed6414f1ce5c53b58c0ed3c8237
Martin <43532302+noicht@users.noreply.github.com>
2020-02-24 13:58:36 -0300

    Add files via upload
    A simple redactor I made in Ruby

---
+++ simple_redactor.rb
@@ -0,0 +1,14 @@
+puts "Enter text: "
+text = gets.chomp
+puts "Enter words to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word == redact
+    print "REDACTED "
+  else
+   print word + " "
+  end
+end
